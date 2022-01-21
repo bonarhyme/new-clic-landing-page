2c172f4d3f33e469b2246b7b709e6cb6e11d94b3
Bonaventure <Bonarhyme@gmail.com>
2022-01-21 12:19:05 +0100

    fixed images not found bug

--- variables/data.js
+++ variables/data.js
@@ -1,19 +1,10 @@
-import logo from "../images/logo.png";
-import feed from "../images/feed.png";
-import leaf from "../images/leaf.png";
-import supporters from "../images/supporters.PNG";
-import image1 from "../images/space1.jpg";
-import image2 from "../images/space2.jpg";
-import image3 from "../images/space3.jpg";
-import face1 from "../images/face1.jpg";
-import face2 from "../images/face2.jpg";
-import face3 from "../images/face3.jpg";
-import wedo1 from "../images/wedo1.jpg";
-import wedo2 from "../images/wedo2.jpg";
-import wedo3 from "../images/wedo3.jpg";
+import logo from "../public/logo.png";
+import feed from "../public/feed.PNG";
+import leaf from "../public/leaf.PNG";
+import supporters from "../public/supporters.PNG";
 
-import target from "../images/target.png";
-import influence from "../images/influence.png";
+import target from "../public/target.png";
+import influence from "../public/influence.png";
 
 export const appData = {
   logo: logo,
@@ -23,23 +14,4 @@ export const appData = {
   target,
   influence,
   companyName: "Lorem ipSum",
-  image1,
-  image2,
-  image3,
-  face1,
-  face2,
-  face3,
-  wedo1,
-  wedo2,
-  wedo3,
-  people: {
-    name1: {
-      name: "Peter Brook",
-      location: "Uyo, Nigeria",
-      type: "Services",
-      business: "Python Tutorials",
-      price: "$25 - $5000",
-      picture: face1,
-    },
-  },
 };
