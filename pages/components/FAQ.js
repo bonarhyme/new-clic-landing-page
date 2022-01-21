710bdda7f2b8934ca8a2e14431708b43165802ed
Bonaventure <Bonarhyme@gmail.com>
2022-01-21 11:17:10 +0100

    Added faq

---
+++ pages/components/FAQ.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Accordion, Container } from "react-bootstrap";
+
+const FAQ = () => {
+  return (
+    <article className="faq py-5">
+      <Container>
+        <h2 className="text-center">FAQs</h2>
+        <Accordion defaultActiveKey="0" flush className="py-3">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>What is Clik?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>How does Clik work?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>How can Clik help you?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
+    </article>
+  );
+};
+
+export default FAQ;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { SSRProvider } from "react-bootstrap";
 import ClikNav from "./components/ClikNav";
+import FAQ from "./components/FAQ";
 import GetInTouch from "./components/GetInTouch";
 import Header from "./components/Header";
 import LetsDo from "./components/LetsDo";
@@ -27,6 +28,7 @@ export default function Home() {
           <Why />
           <GetInTouch />
           <LetsDo />
+          <FAQ />
         </main>
       </div>
     </SSRProvider>
