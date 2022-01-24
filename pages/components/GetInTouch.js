dae18762a43a65da254ed0ae4c4bc8b5945bb366
Bonaventure <Bonarhyme@gmail.com>
2022-01-24 13:44:58 +0100

    Added animations on scroll

--- pages/components/GetInTouch.js
+++ pages/components/GetInTouch.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 import { BsFillPeopleFill, BsQuestionLg } from "react-icons/bs";
 
 import { HiLightBulb } from "react-icons/hi";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const GetInTouch = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  }, []);
+
   return (
     <Container className="py-5">
       <h2 className="text-center">Get In Touch</h2>
       <Row className="justify-content-center align-items-center">
         <Col md={4}>
-          <article className="my-card bg-pink">
+          <article className="my-card bg-pink" data-aos="fade-right">
             <HiLightBulb size={75} color="black" />
             <address>georgia.young@example.com</address>
             <p>
@@ -23,7 +30,7 @@ const GetInTouch = () => {
           </article>
         </Col>
         <Col md={4}>
-          <article className="my-card green big">
+          <article className="my-card green big" data-aos="zoom-in">
             <BsFillPeopleFill size={75} color="white" />
             <address className="white">georgia.young@example.com</address>
             <p>
@@ -35,7 +42,7 @@ const GetInTouch = () => {
           </article>
         </Col>
         <Col md={4}>
-          <article className="my-card bg-pink">
+          <article className="my-card bg-pink" data-aos="fade-left">
             <HiLightBulb size={75} color="black" />
             <address>georgia.young@example.com</address>
             <p>
--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,51 +1,61 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { appData } from "../../variables/data";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const Header = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  }, []);
   return (
     <header>
       <Container className="p-5">
         <Row>
           <Col md={6} xs={12} className="my-5">
-            <h1 className="clik">
-              <strong>Clik</strong>
-            </h1>
-            <p>Be part of your local community</p>
-            <form>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email Address"
-              />
-              <button type="submit">Join Wait List</button>
-            </form>
-            <Form.Text className="text-muted">
-              Clik will soon be available, join wait list to stay informed
-            </Form.Text>
+            <div data-aos="fade-right">
+              <h1 className="clik">
+                <strong>Clik</strong>
+              </h1>
+              <p>Be part of your local community</p>
+              <form>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address"
+                />
+                <button type="submit">Join Wait List</button>
+              </form>
+              <Form.Text className="text-muted">
+                Clik will soon be available, join wait list to stay informed
+              </Form.Text>
+            </div>
           </Col>
           <Col md={6} xs={12}>
-            <div className="leaf-feed-container">
-              <div className="leaf">
-                <Image
-                  src={appData.leaf}
-                  width={70}
-                  height={70}
-                  objectFit="contain"
-                  alt=""
-                />
-              </div>
-              <div className="feed">
-                <Image
-                  src={appData.feed}
-                  width={300}
-                  height={500}
-                  objectFit="contain"
-                  alt="Clik feed sample"
-                  className="feed-image"
-                />
+            <div data-aos="fade-left">
+              <div className="leaf-feed-container">
+                <div className="leaf">
+                  <Image
+                    src={appData.leaf}
+                    width={70}
+                    height={70}
+                    objectFit="contain"
+                    alt=""
+                  />
+                </div>
+                <div className="feed">
+                  <Image
+                    src={appData.feed}
+                    width={300}
+                    height={500}
+                    objectFit="contain"
+                    alt="Clik feed sample"
+                    className="feed-image"
+                  />
+                </div>
               </div>
             </div>
           </Col>
