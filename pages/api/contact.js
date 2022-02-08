4eed768cef85c315c57fff1804d7cfb7362277d7
Bonaventure <Bonarhyme@gmail.com>
2022-02-08 23:45:55 +0100

    :Removed test for .env

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,32 +3,31 @@ import nodemailer from "nodemailer";
 export default async function handler(req, res) {
   const { email, text } = req.body;
 
-  res.send({ message: process.env.SUPPORT_PASSWORD });
-  // const transporter = nodemailer.createTransport({
-  //   service: "Godaddy",
+  const transporter = nodemailer.createTransport({
+    service: "Godaddy",
 
-  //   auth: {
-  //     user: "info@clikplatform.nl",
-  //     pass: process.env.SUPPORT_PASSWORD,
-  //   },
-  // });
-  // const mailData = {
-  //   from: email,
-  //   to: email,
-  //   subject: email.split("@")[0],
-  //   text,
-  // };
-  // transporter.sendMail(mailData, function (err, info) {
-  //   if (err) {
-  //     console.log(err);
-  //     return res
-  //       .status(400)
-  //       .send({ message: "An error occured! Please retry!" });
-  //   } else {
-  //     console.log(info);
-  //     return res
-  //       .status(200)
-  //       .send({ message: "Message sent to us succesfully!" });
-  //   }
-  // });
+    auth: {
+      user: "info@clikplatform.nl",
+      pass: process.env.SUPPORT_PASSWORD,
+    },
+  });
+  const mailData = {
+    from: email,
+    to: email,
+    subject: email.split("@")[0],
+    text,
+  };
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) {
+      console.log(err);
+      return res
+        .status(400)
+        .send({ message: "An error occured! Please retry!" });
+    } else {
+      console.log(info);
+      return res
+        .status(200)
+        .send({ message: "Message sent to us succesfully!" });
+    }
+  });
 }
